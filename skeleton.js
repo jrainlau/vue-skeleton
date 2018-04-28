50865f02e24a22b93e8ee20dbed11eee061f746f
JrainLau <jrainlau@163.com>
2018-04-28 11:33:49 +0800

    feat: enable html minify

--- skeleton.js
+++ skeleton.js
@@ -1,11 +1,18 @@
 
 const fs = require('fs')
+const htmlMinifier = require('html-minifier')
 const { resolve } = require('path')
+
 const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
+
 const renderer = createBundleRenderer(resolve(__dirname, './dist/skeleton.json'), {
   template: fs.readFileSync(resolve(__dirname, './tpl.html'), 'utf-8')
 })
 
 renderer.renderToString({}, (err, html) => {
+  html = htmlMinifier.minify(html, {
+    collapseWhitespace: true,
+    minifyCSS: true
+  })
   fs.writeFileSync('skeleton.html', html, 'utf-8')
 })
--- src/Skeleton.vue
+++ src/Skeleton.vue
@@ -4,14 +4,6 @@
   </div>
 </template>
 
-<script>
-export default {
-  mounted () {
-    console.log('666666')
-  }
-}
-</script>
-
 <style scoped>
 .skeleton {
   color: aqua;
